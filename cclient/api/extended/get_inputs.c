30ab1a30224d7a450e8bf8d250320f8b736a89ec
YangHau <vulxj0j8j8@gmail.com>
2019-05-27 00:19:03 +0800

    iota_client_get_inputs enhancement
    fetch 5 addresses with `iota_client_get_balances` each time, until reaching threshold
    or the balance is not enough for the asked threshold.

--- cclient/api/extended/get_inputs.c
+++ cclient/api/extended/get_inputs.c
@@ -15,49 +15,78 @@ retcode_t iota_client_get_inputs(iota_client_service_t const* const serv, flex_t
   retcode_t ret_code = RC_ERROR;
   hash243_queue_entry_t* q_iter = NULL;
   size_t counter = 0;
-  get_balances_req_t* balances_req = get_balances_req_new();
-  get_balances_res_t* balances_res = get_balances_res_new();
-
-  log_debug(client_extended_logger_id, "[%s:%d]\n", __func__, __LINE__);
-  if (!balances_req || !balances_res) {
-    ret_code = RC_OOM;
-    log_error(client_extended_logger_id, "%s create get balances request or response object failed: %s\n", __func__,
-              error_2_string(ret_code));
-    return ret_code;
-  }
+  get_balances_req_t* balances_req = NULL;
+  get_balances_res_t* balances_res = NULL;
+  bool balance_sufficient = false;
 
   out_inputs->total_balance = 0;
 
+  hash243_queue_t addresses;
+  hash243_queue_entry_t* addresses_q_iter = NULL;
   // get address list
-  ret_code = iota_client_get_new_address(serv, seed, addr_opt, &balances_req->addresses);
+  ret_code = iota_client_get_new_address(serv, seed, addr_opt, &addresses);
   if (ret_code) {
     log_error(client_extended_logger_id, "%s get new address failed: %s\n", __func__, error_2_string(ret_code));
     goto done;
   }
 
-  balances_req->threshold = 100;  // currently `100` should be used
-  ret_code = iota_client_get_balances(serv, balances_req, balances_res);
-  if (ret_code == RC_OK && balances_req->addresses) {
-    // expect balance value is in order.
-    input_t tmp_input = {};
-    CDL_FOREACH(balances_req->addresses, q_iter) {
-      if (out_inputs->total_balance < threshold) {
-        tmp_input.balance = get_balances_res_balances_at(balances_res, counter);
-        tmp_input.key_index = counter;
-        tmp_input.security = addr_opt.security;
-        memcpy(tmp_input.address, q_iter->hash, FLEX_TRIT_SIZE_243);
-        if ((ret_code = inputs_append(out_inputs, &tmp_input)) != RC_OK) {
-          log_error(client_extended_logger_id, "%s get new address failed: %s\n", __func__, error_2_string(ret_code));
-          goto done;
-        }
+  for (uint64_t addr_index = addr_opt.start; addr_index < addr_opt.total;
+       (addr_index + 5 < addr_opt.total) ? (addr_index += 5) : (addr_opt.total - 1)) {
+    balances_req = get_balances_req_new();
+    balances_res = get_balances_res_new();
+
+    log_debug(client_extended_logger_id, "[%s:%d]\n", __func__, __LINE__);
+    if (!balances_req || !balances_res) {
+      ret_code = RC_OOM;
+      log_error(client_extended_logger_id, "%s create get balances request or response object failed: %s\n", __func__,
+                error_2_string(ret_code));
+      return ret_code;
+    }
+
+    balances_req->threshold = 100;  // currently `100` should be used
+    balances_req->addresses = NULL;
+
+    uint64_t addr_push_index_end = addr_index + 5;
+    flex_trit_t* addr;
+    for (uint64_t addr_push_index = addr_index; addr_push_index < addr_push_index_end; addr_push_index++) {
+      if (addr = hash243_queue_at(addresses, addr_push_index) != NULL) {
+        hash243_queue_push(balances_req->addresses, addr);
+      } else {
+        break;
       }
-      counter++;
     }
-    // check if balance is sufficient
-    if (out_inputs->total_balance < threshold) {
-      ret_code = RC_CCLIENT_INSUFFICIENT_BALANCE;
-      log_warning(client_extended_logger_id, "insufficient balance\n");
+
+    ret_code = iota_client_get_balances(serv, balances_req, balances_res);
+    if (ret_code == RC_OK && balances_req->addresses) {
+      // expect balance value is in order.
+      input_t tmp_input = {};
+      CDL_FOREACH(balances_req->addresses, q_iter) {
+        if (out_inputs->total_balance < threshold) {
+          tmp_input.balance = get_balances_res_balances_at(balances_res, counter);
+          tmp_input.key_index = counter;
+          tmp_input.security = addr_opt.security;
+          memcpy(tmp_input.address, q_iter->hash, FLEX_TRIT_SIZE_243);
+          if ((ret_code = inputs_append(out_inputs, &tmp_input)) != RC_OK) {
+            log_error(client_extended_logger_id, "%s get new address failed: %s\n", __func__, error_2_string(ret_code));
+            goto done;
+          }
+        } else {
+          balance_sufficient = true;
+          break;
+        }
+
+        counter++;
+      }
     }
+
+    get_balances_req_free(&balances_req);
+    get_balances_res_free(&balances_res);
+  }
+
+  // check if balance is sufficient
+  if (balance_sufficient == false) {
+    ret_code = RC_CCLIENT_INSUFFICIENT_BALANCE;
+    log_warning(client_extended_logger_id, "insufficient balance\n");
   }
 
 done:
